49f5833ccd6760b6524ba12102909c59fde0982a
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-06-24 11:11:02 +0200

    Updated content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,34 @@
 export function Content() {
-  return (<main>
-      <div className="weather-app-data">
-          <div>
-              <h1 className="weather-app-city" id="city"> Stockholm</h1>
-              <p className="weather-app-details">
-                  <span id="time"></span> 13:00<br />
-                  <span id="description"></span>
-                  <br />
-                  Humidity: <strong id="humidity"></strong>, Wind: <strong id="wind-speed"></strong>
-              </p>
+  return (
+    <main>
+      <div className='weather-app-data'>
+        <div>
+          <h1 className='weather-app-city' id='city'>
+            {' '}
+            Stockholm
+          </h1>
+          <p className='weather-app-details'>
+            <span id='time'></span> 13:00
+            <br />
+            <span id='description'></span>
+            <br />
+            Humidity: 47% <strong id='humidity'></strong>, Wind:{'3 m/s '}
+            <strong id='wind-speed'></strong>
+          </p>
+        </div>
+        <div className='weather-app-temperature-container'>
+          <div id='icon'>
+            <img
+              src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII='
+              alt='sunny with some clouds'
+            />
           </div>
-          <div className="weather-app-temperature-container">
-              <div id="icon"></div>
-              <div className="weather-app-temperature" id="temperature">12</div>
-              <div className="weatehr-app-unit">°C</div>
+          <div className='weather-app-temperature' id='temperature'>
+            12
           </div>
+          <div className='weatehr-app-unit'>°C</div>
+        </div>
       </div>
-  </main>)
+    </main>
+  );
 }
