9b5e1566d0c9c3ef2f44eed6dd249debe09e6883
Edona Rexhaj <131316928+ShiineCodes@users.noreply.github.com>
2024-06-24 10:31:43 +0200

    Changed the CI environment to publish on Netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 export function Footer() {
   return <footer>
       This project was coded with 🤍 by
-      <a href="" target="_blank">
+      <a href="https://github.com/ShiineCodes" target="_blank">
           Edona Rexhaj,
       </a>
       is
 
-      <a href="" target="_blank"
+      <a href="https://github.com/ShiineCodes/weather-react" target="_blank"
       >open-sourced on Github
       </a>
       and
